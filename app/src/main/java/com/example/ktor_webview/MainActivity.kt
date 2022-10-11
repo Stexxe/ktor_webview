bfd06847d0a371462b1f9e5f3992b6c835ab5323
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2022-10-11 13:40:46 +0300

    Copy web assets recursively

--- app/src/main/java/com/example/ktor_webview/MainActivity.kt
+++ app/src/main/java/com/example/ktor_webview/MainActivity.kt
@@ -11,45 +11,50 @@ import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.routing.*
 import java.io.File
+import java.io.FileNotFoundException
 import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        copyWebResources()
+        copyWebResources("web", filesDir)
 
-        val server = initHttpServer()
-        server.start()
-
-        val webview = findViewById<WebView>(R.id.webview)
-        initWebViewContent(webview)
-    }
-
-    private fun copyWebResources() {
-        val files = assets.list("web")
-
-        files?.forEach { path ->
-            println(path)
-            val input = assets.open("web/$path")
-            val outFile = File(filesDir, path)
-            val outStream = FileOutputStream(outFile)
-            outStream.write(input.readBytes())
-            outStream.close()
-            input.close()
-        }
-    }
-
-    private fun initHttpServer(): ApplicationEngine {
-        return embeddedServer(Netty, 3333) {
+        embeddedServer(Netty, 3333) {
             routing {
                 static("static") {
                     files(filesDir)
                 }
             }
+        }.start(wait = false)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        initWebViewContent(webView)
+    }
+
+    private fun copyWebResources(assetDir: String, outDir: File) {
+        val files = assets.list(assetDir) ?: return
+
+        for (path in files) {
+            val outFile = File(outDir, path)
+            val assetPath = "$assetDir/$path"
+
+            val input = try {
+                assets.open(assetPath).buffered()
+            } catch (_: FileNotFoundException) {
+                // Seems like path points to a directory
+                outFile.mkdir()
+                copyWebResources(assetPath, File(outDir, path))
+                continue
+            }
+
+            FileOutputStream(outFile).use { stream ->
+                stream.write(input.readBytes())
+            }
+
+            input.close()
         }
     }
 
@@ -58,11 +63,6 @@ class MainActivity : AppCompatActivity() {
         webView.apply {
             loadUrl("http://127.0.0.1:3333/static/index.html")
             webViewClient = object : WebViewClient() {
-//                override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-//                    view.loadUrl(url)
-//                    return false
-//                }
-
                 override fun shouldOverrideUrlLoading(
                     view: WebView?,
                     request: WebResourceRequest?
